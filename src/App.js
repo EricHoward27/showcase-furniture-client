34897555b19df6548d6df8f2236eb7d9d6a09f39
eric <erichoward2141@gmail.com>
2021-05-15 13:43:02 -0500

    reactify products to map through items to dry code

--- src/App.js
+++ src/App.js
@@ -5,12 +5,56 @@ import Products from './components/Products'
 import Footer from './components/Footer'
 
 function App() {
+  const productLinks = [
+    { 
+      name: 'Product 1',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    },
+    { 
+      name: 'Product 2',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    },
+    { 
+      name: 'Product 3',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    },
+    { 
+      name: 'Product 4',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    },
+    { 
+      name: 'Product 5',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    },
+    { 
+      name: 'Product 6',
+      price: '39.99',
+      image: 'https://via.placeholder.com/700x400',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+      rating: '★ ★ ★ ★ ☆'
+    }
+  ]
   return (
     <div>
       <Header />
       <div className="container">
         <MastHead />
-        <Products />
+        <Products productLinks={productLinks}/>
       </div>
       <Footer />
     </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
-const Products = () => {
+const Products = ({ productLinks }) => {
   return (
     <div className="row">
+      { productLinks && productLinks.map(({ price, image, description, rating, name }, index) =>
         <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
-                <a href="#!"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
+                <a href="#!"><img className="card-img-top" src={ image } alt="..." /></a>
                 <div className="card-body">
-                    <h4 className="card-title"><a href="#!">Item One</a></h4>
-                    <h5>$24.99</h5>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    <h4 className="card-title"><a href="#!">{ name }</a></h4>
+                    <h5>{ price }</h5>
+                    <p className="card-text">{ description }</p>
                 </div>
-                <div className="card-footer"><small className="text-muted">★ ★ ★ ★ ☆</small></div>
+                <div className="card-footer"><small className="text-muted">{ rating }</small></div>
             </div>
         </div>
-        <div className="col-lg-4 col-md-6 mb-4">
+      )}
+  
+        {/* <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
                 <a href="#!"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
                 <div className="card-body">
@@ -68,7 +71,7 @@ const Products = () => {
                 </div>
                 <div className="card-footer"><small className="text-muted">★ ★ ★ ★ ☆</small></div>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
