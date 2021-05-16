f520cdbc80536d001b303f435ac4a71f71b0deb8
eric <erichoward2141@gmail.com>
2021-05-16 16:11:33 -0500

    added productlinks to homescreen component

--- src/App.js
+++ src/App.js
@@ -9,56 +9,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
 function App() {
-  const productLinks = [
-    { 
-      name: 'Product 1',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '3',
-      numReviews: '7'
-    },
-    { 
-      name: 'Product 2',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '2',
-      numReviews: '11'
-    },
-    { 
-      name: 'Product 3',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '4',
-      numReviews: '4'
-    },
-    { 
-      name: 'Product 4',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '5',
-      numReviews: '9'
-    },
-    { 
-      name: 'Product 5',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '3',
-      numReviews: '2'
-    },
-    { 
-      name: 'Product 6',
-      price: '39.99',
-      image: 'https://via.placeholder.com/700x400',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      rating: '1',
-      numReviews: '13'
-    }
-  ]
+
   return (
     <div>
       <Header />
@@ -66,7 +17,6 @@ function App() {
         <MastHead />
         <Route path="/" component={HomeScreen} exact/>
         <Route path="/product/:id" component={ProductScreen} />
-        <Products productLinks={productLinks}/>
       </main>
       <Footer />
     </div>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import Product from '../components/Products'
 
 const HomeScreen = () => {
+    const productLinks = [
+        { 
+          name: 'Product 1',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '3',
+          numReviews: '7'
+        },
+        { 
+          name: 'Product 2',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '2',
+          numReviews: '11'
+        },
+        { 
+          name: 'Product 3',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '4',
+          numReviews: '4'
+        },
+        { 
+          name: 'Product 4',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '5',
+          numReviews: '9'
+        },
+        { 
+          name: 'Product 5',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '3',
+          numReviews: '2'
+        },
+        { 
+          name: 'Product 6',
+          price: '39.99',
+          image: 'https://via.placeholder.com/700x400',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+          rating: '1',
+          numReviews: '13'
+        }
+      ]
     return (
-      <div>Product Screen</div>
+      <div>
+        <Product productLinks={productLinks}/>
+      </div>
     )
 }
 export default HomeScreen
