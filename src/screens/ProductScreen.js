3525078c8fc081f9506c79d881bbabf7adb39c55
eric <erichoward2141@gmail.com>
2021-05-18 19:19:29 -0500

    build product page layout, next iteration will include product data from api

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,8 +1,56 @@
 import React from 'react'
-
-const ProductScreen = () => {
+import Ratings from '../components/Ratings'
+const ProductScreen = (props) => {
+  const { name, image, ratings, numReviews, price, description, countInStock, id } = props
+  if (!id) {
+    return <div> Product not found</div>
+  }
   return (
-    <div>Product Screen</div>
+    <div>
+      <div className="row top">
+        <div className="col-2">
+          <img src={ image } alt={ name } />
+        </div>
+        <div className="col-1">
+          <ul>
+            <li>
+              <h1>{ name }</h1>
+            </li>
+            <li>
+              <Ratings ratings={ ratings } numReviews={ numReviews }/>
+            </li>
+            <li>Price: ${ price }</li>
+            <li>
+              Description:
+              <p>{ description }</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <div className="row">
+                  <div>Price</div>
+                  <div className="price">${ price }</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Status</div>
+                  <div>
+                    { countInStock > 0 ? ( <span className="success">In Stock</span> ) : ( <span className="error">Unavailable</span> ) }
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className="primary block">Add to Cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default ProductScreen
