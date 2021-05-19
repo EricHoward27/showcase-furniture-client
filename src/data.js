84994fb61213c372264e09c0ed1e9f7788bab061
eric <erichoward2141@gmail.com>
2021-05-18 19:34:34 -0500

    fix layout in product page view

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = {
+  products: [
+    { 
+        id: '1',
+        name: 'Product 1',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '3',
+        numReviews: '7'
+      },
+      { 
+        id: '2',
+        name: 'Product 2',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '2',
+        numReviews: '11'
+      },
+      { 
+        id: '3',
+        name: 'Product 3',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '4',
+        numReviews: '4'
+      },
+      { 
+        id: '4',
+        name: 'Product 4',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '5',
+        numReviews: '9'
+      },
+      { 
+        id: '5',
+        name: 'Product 5',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '3',
+        numReviews: '2'
+      },
+      { 
+        id: '6',
+        name: 'Product 6',
+        price: '39.99',
+        image: 'https://via.placeholder.com/700x400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        rating: '1',
+        numReviews: '13'
+      }
+  ]
+}
+export default data
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import Ratings from '../components/Ratings'
+import data from '../data'
 const ProductScreen = (props) => {
-  const { name, image, ratings, numReviews, price, description, countInStock, id } = props
-  if (!id) {
+  const product = data.products.find((p) => p.id === props.match.params.id)
+  if (!product) {
     return <div> Product not found</div>
   }
   return (
     <div>
       <div className="row top">
         <div className="col-2">
-          <img src={ image } alt={ name } />
+          <img src={ product.image } alt={ product.name } />
         </div>
         <div className="col-1">
           <ul>
             <li>
-              <h1>{ name }</h1>
+              <h1>{ product.name }</h1>
             </li>
             <li>
-              <Ratings ratings={ ratings } numReviews={ numReviews }/>
+              <Ratings ratings={ product.ratings } numReviews={ product.numReviews }/>
             </li>
-            <li>Price: ${ price }</li>
+            <li>Price: ${ product.price }</li>
             <li>
               Description:
-              <p>{ description }</p>
+              <p>{ product.description }</p>
             </li>
           </ul>
         </div>
@@ -32,14 +33,14 @@ const ProductScreen = (props) => {
               <li>
                 <div className="row">
                   <div>Price</div>
-                  <div className="price">${ price }</div>
+                  <div className="price">${ product.price }</div>
                 </div>
               </li>
               <li>
                 <div className="row">
                   <div>Status</div>
                   <div>
-                    { countInStock > 0 ? ( <span className="success">In Stock</span> ) : ( <span className="error">Unavailable</span> ) }
+                    { product.countInStock > 0 ? ( <span className="success">In Stock</span> ) : ( <span className="error">Unavailable</span> ) }
                   </div>
                 </div>
               </li>
